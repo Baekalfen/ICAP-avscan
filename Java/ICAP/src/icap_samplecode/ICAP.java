eac2793c1374307a7cfddd89d7fe41a49427bb20
sergey.sviridov01 <sergey.sviridov@ctco.lv>
2018-07-25 17:50:23 +0300

    organize imports

--- Java/ICAP/src/icap_samplecode/ICAP.java
+++ Java/ICAP/src/icap_samplecode/ICAP.java
@@ -1,6 +1,13 @@
 package icap_samplecode;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.Charset;
 import java.util.Arrays;
